a41b23b6eeab950bb1071231f5863158b67f6e89
ArtLevel <algitIT@rambler.ru>
2023-08-07 17:59:17 +0300

    02-03 complete

--- 02/03.js
+++ 02/03.js
@@ -1,4 +1,8 @@
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
