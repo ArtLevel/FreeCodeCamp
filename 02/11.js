59480194edb5d893d090fc18642b74d44b12c235
ArtLevel <algitIT@rambler.ru>
2023-11-13 11:22:13 +0300

    Task Completed

--- 02/11.js
+++ 02/11.js
@@ -1,8 +1,26 @@
-let a = 5;
-let b = 12;
-let c = 4.6;
+const f1 = () => {
+	let a = 5;
+	let b = 12;
+	let c = 4.6;
 
 // Only change code below this line
-a *= 5;
-b *= 3;
-c *= 10;
+	a *= 5;
+	b *= 3;
+	c *= 10;
+}
+const f2 = () => {
+	let a = 48;
+	let b = 108;
+	let c = 33;
+
+// Only change code below this line
+	a /= 12;
+	b /= 4;
+	c /= 11;
+}
+const f3 = () => {
+	const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+}
+const f4 = () => {
+	const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+}
